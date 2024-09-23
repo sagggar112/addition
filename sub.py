2fc8459121cba80c87e6baf48a2392e6456890a5
sagggar112 <sagardanai1@gmail.com>
2024-09-22 21:20:51 -0600

    subraction file added

---
+++ sub.py
@@ -0,0 +1,9 @@
+# Define two variables to hold the numbers
+num1 = 10
+num2 = 5
+
+# Subtract the numbers and store the result in a variable
+difference = num1 - num2
+
+# Print the result
+print("The difference of", num1, "and", num2, "is:", difference)
