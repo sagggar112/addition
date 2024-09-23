e87ed7df333659f2c613fb90a87d6cab5e1ee218
sagggar112 <sagardanai1@gmail.com>
2024-09-22 20:56:56 -0600

    file added

---
+++ add.py
@@ -0,0 +1,9 @@
+# Define two variables to hold the numbers
+num1 = 10
+num2 = 5
+
+# Add the numbers and store the result in a variable
+sum = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", sum)
